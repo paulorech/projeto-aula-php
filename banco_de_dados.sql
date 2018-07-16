4c59e6012f726cf8eb7f9b6f940fd8aee8d218bf
Paulo Rech <rech.paulog@gmail.com>
2018-07-16 19:40:23 -0300

    banco de dados

---
+++ banco_de_dados.sql
@@ -0,0 +1,24 @@
+CREATE TABLE pessoa (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    email varchar(255),
+    sexo varchar(1),
+    data_nascimento timestamp,
+    uf varchar(255),
+    cidade varchar(255),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE uf (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    uf_id int NOT NULL,
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE cidade (
+    id int NOT NULL AUTO_INCREMENT,
+    nome varchar(255),
+    sigla varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+)
