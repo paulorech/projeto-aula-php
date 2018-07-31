318fec4126d61c3611cacf93bb57bba26cc03721
Paulo Rech <rech.paulog@gmail.com>
2018-07-30 22:13:38 -0300

    Separação dos metodos de deletar no php e inclusão de um modal para deletar estilizado

--- comum/footer.php
+++ comum/footer.php
@@ -8,13 +8,29 @@
 </footer>
 <?php /* FIM RODAPE */ ?>
 
-<?php /* INICIO SCRIPTS ADICIONAIS */ ?>
+
+
+<div id="modal-delete" class="modal fade" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger btn-sim">Sim</button>
+        <button type="button" class="btn btn-default btn-nao">Não</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php /* SCRIPTS FINAIS */ ?>
 <script src="<?php echo $SITE_URL . "/static/vendor/jquery/jquery.min.js"; ?>"></script>
 <script src="<?php echo $SITE_URL . "/static/vendor/bootstrap/js/bootstrap.bundle.min.js"; ?>"></script>
 <script src="<?php echo $SITE_URL . "/static/vendor/jquery-easing/jquery.easing.min.js"; ?>"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script>
 <script src="<?php echo $SITE_URL . "/static/js/sb-admin.min.js"; ?>"></script>
 <script src="<?php echo $SITE_URL . "/static/js/home.js"; ?> "></script>
-<?php /* FIM SCRIPTS ADICIONAIS */ ?>	
-
+<?php /* SCRIPTS FINAIS */ ?>
 </body>
+</html>
